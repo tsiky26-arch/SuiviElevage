57ad3f7594a84cb1362518c363e62292e19bda7a
tsiky26-arch <tsikyherinomeniavo@gmail.com>
2025-02-03 22:36:46 +0300

    liste animaux dispo poour la vente

---
+++ app/models/ListeAnimauxDispoVendreModel.php
@@ -0,0 +1,33 @@
+<?php 
+namespace app\models;
+use Flight;
+
+class ListeAnimauxDispoVendreModel{
+    private $db;
+
+    public function __construct() {
+        
+    }
+      public  function listeDispoAVendre($idU){
+        try {
+            $this->db = Flight::db();
+            $statut = 'vendu';
+            $query = "SELECT * FROM elevage_AnimauxEleve WHERE idUtilisateur= ? AND statut = ? ";
+            
+            $stmt = $this->db->prepare($query);
+      
+            $stmt->bindValue(1,$idU);
+            $stmt->bindValue(2,$statut);
+
+            
+            $stmt->execute();
+            
+        } catch (\Exception $e) {
+            echo "une erreur c'est produite" .$e->getMessage();
+        }
+      } 
+}
+
+
+
+?>
